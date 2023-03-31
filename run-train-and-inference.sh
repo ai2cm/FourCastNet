f17bb5bb4817661b83b0cb131f0c57cb3c492242
Gideon Dresdner <gideond@gmail.com>
2023-03-31 20:47:16 +0000

    re-enable inference

--- run-train-and-inference.sh
+++ run-train-and-inference.sh
@@ -8,5 +8,5 @@ export WANDB_JOB_TYPE=training
 torchrun --nproc_per_node 1 /opt/ERA5_wind/train.py --yaml_config $YAML_CONFIG --config $CONFIG_SETTING
 
 # run inference
-#export WANDB_JOB_TYPE=inference
-#python /opt/ERA5_wind/inference/inference.py --yaml_config $YAML_CONFIG --config $CONFIG_SETTING --override_dir /output --weights /trainedmodel/$CONFIG_SETTING/00/training_checkpoints/best_ckpt.tar --vis
+export WANDB_JOB_TYPE=inference
+python /opt/ERA5_wind/inference/inference.py --yaml_config $YAML_CONFIG --config $CONFIG_SETTING --override_dir /output --weights /trainedmodel/$CONFIG_SETTING/00/training_checkpoints/best_ckpt.tar --vis
