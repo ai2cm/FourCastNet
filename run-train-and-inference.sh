1401281e757bacf808338f716b560f5861b62276
Gideon Dresdner <gideond@gmail.com>
2023-03-31 19:54:03 +0000

    add script

---
+++ run-train-and-inference.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+YAML_CONFIG=/configmount/config.yaml
+CONFIG_SETTING=full_field
+
+# run training
+export WANDB_JOB_TYPE=training
+torchrun --nproc_per_node 8 /opt/ERA5_wind/train.py --yaml_config $YAML_CONFIG --config $CONFIG_SETTING
+
+# run inference
+export WANDB_JOB_TYPE=inference
+python /opt/ERA5_wind/inference/inference.py --yaml_config $YAML_CONFIG --config $CONFIG_SETTING --override_dir /output --weights /trainedmodel/$CONFIG_SETTING/00/training_checkpoints/best_ckpt.tar --vis
