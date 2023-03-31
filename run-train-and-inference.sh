581e36dd4787ba14f727136bf07bd589c549c4d5
Gideon Dresdner <gideond@gmail.com>
2023-03-31 23:58:00 +0000

    match output dirs

--- run-train-and-inference.sh
+++ run-train-and-inference.sh
@@ -9,4 +9,5 @@ torchrun --nproc_per_node 1 /opt/ERA5_wind/train.py --yaml_config $YAML_CONFIG -
 
 # run inference
 export WANDB_JOB_TYPE=inference
-python /opt/ERA5_wind/inference/inference.py --yaml_config $YAML_CONFIG --config $CONFIG_SETTING --override_dir /output --weights /trainedmodel/$CONFIG_SETTING/00/training_checkpoints/best_ckpt.tar --vis
+#TODO(gideond) factor out /output
+python /opt/ERA5_wind/inference/inference.py --yaml_config $YAML_CONFIG --config $CONFIG_SETTING --override_dir /output --weights /output/$CONFIG_SETTING/00/training_checkpoints/best_ckpt.tar --vis
