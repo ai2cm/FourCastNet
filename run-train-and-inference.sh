50fe67141d3b24c5f34b4e79f2d83b7f5cc3cba4
Gideon Dresdner <gideond@gmail.com>
2023-04-03 19:30:11 +0000

    foobar

--- run-train-and-inference.sh
+++ run-train-and-inference.sh
@@ -7,7 +7,11 @@ CONFIG_SETTING=full_field
 export WANDB_JOB_TYPE=training
 torchrun --nproc_per_node 1 /opt/ERA5_wind/train.py --yaml_config $YAML_CONFIG --config $CONFIG_SETTING
 
+echo ===============================================================================
+echo ======================FINISHED TRAINING STARTING INFERENCE=====================
+echo ===============================================================================
+
 # run inference
-export WANDB_JOB_TYPE=inference
+#export WANDB_JOB_TYPE=inference
 #TODO(gideond) factor out /output
-python /opt/ERA5_wind/inference/inference.py --yaml_config $YAML_CONFIG --config $CONFIG_SETTING --override_dir /output --weights /output/$CONFIG_SETTING/00/training_checkpoints/best_ckpt.tar --vis
+#python /opt/ERA5_wind/inference/inference.py --yaml_config $YAML_CONFIG --config $CONFIG_SETTING --override_dir /output --weights /output/$CONFIG_SETTING/00/training_checkpoints/best_ckpt.tar --vis
