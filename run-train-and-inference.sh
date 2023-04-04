d8b1b3ff94147257ba8f73fc92c24da2b966b6b6
Gideon Dresdner <gideond@gmail.com>
2023-04-04 21:05:08 +0000

    move run group to vcm yaml config

--- run-train-and-inference.sh
+++ run-train-and-inference.sh
@@ -4,8 +4,6 @@ YAML_CONFIG=$1
 CONFIG_SETTING=$2
 NPROC_PER_NODE=$3
 
-export WANDB_RUN_GROUP="tr-and-inf"
-
 # run training
 export WANDB_JOB_TYPE=training
 torchrun --nproc_per_node $NPROC_PER_NODE /opt/ERA5_wind/train.py --yaml_config $YAML_CONFIG --config $CONFIG_SETTING
